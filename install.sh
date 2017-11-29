e26169aeffd640ad8a44a35a988afa6e75c4b912
aiya000 <aiya000.develop@gmail.com>
2017-11-29 22:42:52 +0900

    Add a notification to install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ if [[ ! -d $HOME/.config/doromochi ]] ; then
     mkdir -p $HOME/.config/doromochi || exit 1
 fi
 
+echo 'copying resources, please wait...'
 cp -r images ~/.config/doromochi/images && etlas install
