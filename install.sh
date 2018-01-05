48944c0f51c367855d27347db7fec388357c2073
aiya000 <aiya000.develop@gmail.com>
2018-01-05 23:39:59 +0900

    Create sounds directory with the license

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ ! -d $HOME/.config/doromochi ]] ; then
-    mkdir -p $HOME/.config/doromochi || exit 1
+    mkdir -p "$HOME/.config/doromochi" || exit 1
 fi
 
 echo 'copying resources, please wait...'
-cp -r images ~/.config/doromochi && etlas install
+cp -r images sounds ~/.config/doromochi && etlas install
